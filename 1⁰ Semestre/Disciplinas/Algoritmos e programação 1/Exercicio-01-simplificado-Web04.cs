80583cd401af470525b0f8b77b89ea18c4de6ffd
hexdien <hexdien@proton.me>
2026-05-06 23:36:11 -0300

    Resolução do problema simplificando o código

---
+++ 1⁰ Semestre/Disciplinas/Algoritmos e programação 1/Exercicio-01-simplificado-Web04.cs
@@ -0,0 +1,35 @@
+﻿
+
+Continue cont = new Continue();
+
+cont.digitarNumero();
+
+class Continue{
+int num;
+List<int> lista = new List<int>();
+
+  public void digitarNumero()
+    {
+        while (true)
+        {
+            num = int.TryParse(Console.ReadLine(), out int value)
+                ? value
+                : 0;
+
+            if (num < 0)
+            {
+                Console.WriteLine("Número negativo digitado.");
+                break;
+            }
+
+            lista.Add(num);
+
+            continue;
+        }
+        Console.WriteLine($"Total da lista somado: {this.lista.Sum()} ");
+        Console.WriteLine($"Média da lista : {this.lista.Average()} ");
+        Console.WriteLine("Fim do programa");
+    }
+
+
+}
